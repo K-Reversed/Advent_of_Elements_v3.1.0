87ddb264e36c2288080a1cfb3fbd4352a212759b
K-Reversed <99205481+K-Reversed@users.noreply.github.com>
2022-04-14 11:23:39 -0400

    Added Game States
    increased the number of constants within enum GameStates.java by 12.

--- src/GameStates.java
+++ src/GameStates.java
@@ -1,2 +1,14 @@
 public enum GameStates {
+  TitleScreen(),
+  SettingsScreen(),
+  GameMap0(),
+  GameMap1(),
+  GameMap2(),
+  GameMap3(),
+  GameMap4(),
+  GameMap5(),
+  GameMap6(),
+  GameMap7(),
+  GameMap8(),
+  GameMap9()
 }
